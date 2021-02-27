f9ffe1eb7ebbc21aad3783a4406943b239e19d60
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-02-27 19:59:03 +0400

    feat(web): add global styles #3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import { AppProps } from "next/app"
 import { ThemeProvider } from "styled-components"
 
 import { theme } from "../src/theme/theme"
+import { GlobalStyle } from "../src/theme/global-style"
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => (
 	<ThemeProvider theme={theme}>
+    <GlobalStyle />
 		<Component {...pageProps} />
 	</ThemeProvider>
 )
