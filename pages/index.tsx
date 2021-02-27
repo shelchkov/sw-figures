cabc60412cfa4ae727593d4d92f9ca785e18f521
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-02-27 19:24:31 +0400

    feat(web): add layout component #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement } from "react"
 import styled from "styled-components"
 
+import { Layout } from "../src/components/ui/layout"
+
 const Title = styled.p`
 	color: ${(p): string => p.theme.colors["accent-800"]};
 `
 
-const Index = (): ReactElement => {
-	return <Title>Index</Title>
-}
+const Index = (): ReactElement => (
+	<Layout title="Home">
+		<Title>Index</Title>
+	</Layout>
+)
 
 export default Index
---
+++ src/components/ui/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from "react"
+import Head from "next/head"
+
+import { ComponentWithChildren } from "../../utils/types"
+
+type Props = ComponentWithChildren<{
+	title: string
+}>
+
+export const Layout = ({ title, children }: Props): ReactElement => (
+	<>
+		<Head>
+			<title>{title}</title>
+			<meta
+				name="viewport"
+				content="initial-scale=1.0, width=device-width"
+			/>
+		</Head>
+
+		{children}
+	</>
+)
