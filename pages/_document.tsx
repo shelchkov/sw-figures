2d32b4cf480c96734d639b0d7d2bba06c597bf87
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-02-27 13:12:58 +0400

    feat(web): add server side styles #1

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { DocumentContext, DocumentInitialProps, DocumentProps, Head, Html, Main, NextScript } from "next/document"
+import React from "react"
+import { ServerStyleSheet } from "styled-components"
+
+class MyDocument extends Document<DocumentProps> {
+  static async getInitialProps({ renderPage }: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet  = new ServerStyleSheet()
+
+    const page = await renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
+
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styles: styleTags }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.styles}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
