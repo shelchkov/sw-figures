27b15941751f24e855b5db7536d142b014975ac4
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-02-28 14:33:50 +0400

    feat(web): add menu logo link #6

--- src/components/ui/heading.ts
+++ src/components/ui/heading.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components"
 
-export const Heading900 = styled.p`
+const Heading = styled.p`
 	margin: 0;
 	color: ${(p): string => p.theme.colors["accent-900"]};
 	font-weight: 700;
 	font-size: 56px;
 	line-height: 112%;
+`
 
+export const Heading900 = styled(Heading)`
 	@media (min-width: ${(p): string => p.theme.breakpoints.sm}) {
 		font-size: 82px;
 	}
@@ -16,7 +18,7 @@ export const Heading900 = styled.p`
 	}
 `
 
-export const Heading800 = styled(Heading900)`
+export const Heading800 = styled(Heading)`
 	font-size: 64px;
 
 	@media (min-width: ${(p): string => p.theme.breakpoints.xl}) {
@@ -24,7 +26,7 @@ export const Heading800 = styled(Heading900)`
 	}
 `
 
-export const Heading700 = styled(Heading900)`
+export const Heading700 = styled(Heading)`
 	font-size: 44px;
 
 	@media (min-width: ${(p): string => p.theme.breakpoints.xl}) {
@@ -32,7 +34,7 @@ export const Heading700 = styled(Heading900)`
 	}
 `
 
-export const Heading600 = styled(Heading900)`
+export const Heading600 = styled(Heading)`
 	font-size: 28px;
 	line-height: 132%;
 
--- src/components/ui/paragraph.ts
+++ src/components/ui/paragraph.ts
@@ -1,17 +1,36 @@
 import styled from "styled-components"
 
-export const Paragraph900 = styled.p`
+const Paragraph = styled.p`
 	margin: 0;
 	font-weight: 600;
 	font-size: 24px;
 	line-height: 142%;
 	color: ${(p): string => p.theme.colors["accent-900"]};
+`
 
+export const Paragraph900 = styled(Paragraph)`
 	@media (min-width: ${(p): string => p.theme.breakpoints.sm}) {
 		font-size: 28px;
 	}
 
 	@media (min-width: ${(p): string => p.theme.breakpoints.xl}) {
 		font-size: 32px;
+	}
+`
+
+export const Paragrah800 = styled(Paragraph)`
+	font-size: 21px;
+
+	@media (min-width: ${(p): string => p.theme.breakpoints.md}) {
+		font-size: 24px;
+	}
+`
+
+export const Paragrah400 = styled(Paragraph)`
+	font-size: 16px;
+	line-height: 154%;
+
+	@media (min-width: ${(p): string => p.theme.breakpoints.md}) {
+		font-size: 18px;
 	}
 `
