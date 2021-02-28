4db0ec94acea6d70fdc0e1f1d658c1cdb2ccbad8
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-02-28 10:41:15 +0400

    feat(web): add favicon and description #8

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -17,6 +17,17 @@ export const Layout = ({ title, children }: Props): ReactElement => (
 				name="viewport"
 				content="initial-scale=1.0, width=device-width"
 			/>
+			<link rel="icon" href="/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link rel="manifest" href="/site.webmanifest" />
+			<meta
+				name="description"
+				content="Find the latest products for the biggest fans of the iconic saga"
+			/>
 		</Head>
 
 		<Menu />
