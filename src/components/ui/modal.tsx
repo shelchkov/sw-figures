b361d1f539b9d4230a9fc57e84306df23b2bb216
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2021-03-13 14:28:38 +0400

    feat(web): add modal #21

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,61 @@
+import React, { ReactElement } from "react"
+import ReactDOM from "react-dom"
+import styled from "styled-components"
+
+import { ComponentWithChildren } from "../../utils/types"
+import { isBrowser } from "../../utils/utils"
+
+type Props = ComponentWithChildren<{
+	isOpen: boolean
+	close: () => void
+}>
+
+export const modalId = "modal-container"
+
+const Backdrop = styled.div`
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+const Container = styled.div`
+	position: fixed;
+	left: 50%;
+	top: 50%;
+	width: 400px;
+	height: 400px;
+	padding: 32px;
+	background-color: ${(p): string => p.theme.colors["bg-100"]};
+	border: 1px solid ${(p): string => p.theme.colors["stroke-100"]};
+	border-radius: 16px;
+	transform: translate(-50%, -50%);
+`
+
+export const Modal = ({
+	children,
+	isOpen,
+	close,
+}: Props): ReactElement => {
+	if (!isOpen || !isBrowser) {
+		return <></>
+	}
+
+	const modalContainer = document.querySelector(`div#${modalId}`)
+
+	if (!modalContainer) {
+		return <></>
+	}
+
+	return ReactDOM.createPortal(
+		<>
+			<Backdrop onClick={close}></Backdrop>
+			<Container>{children}</Container>
+		</>,
+		modalContainer,
+	)
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,4 @@
 export const getPrice = (price: number): string =>
 	(price / 100).toFixed(2)
+
+export const isBrowser = typeof document === "object"
